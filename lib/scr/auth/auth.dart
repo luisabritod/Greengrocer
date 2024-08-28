c756f408eb34e886bb495f0aef2c2a42634a70f4
Luísa Brito <lulubritodias@gmail.com>
2024-08-28 19:36:09 -0300

    first commit

---
+++ lib/scr/auth/auth.dart
@@ -0,0 +1,3 @@
+export 'sign_in_screen.dart';
+export 'sign_up_screen.dart';
+export 'components/components.dart';
---
+++ lib/scr/auth/components/custom_text_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
+    super.key,
+    required this.icon,
+    required this.hintText,
+    required this.labelText,
+    required this.keyboardType,
+    this.isPassword = false,
+  });
+
+  final IconData icon;
+  final String hintText;
+  final String labelText;
+  final TextInputType keyboardType;
+  final bool isPassword;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool isObscure = false;
+
+  @override
+  void initState() {
+    isObscure = widget.isPassword;
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15.0),
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        obscureText: isObscure,
+        decoration: InputDecoration(
+          labelText: widget.labelText,
+          hintText: widget.hintText,
+          isDense: true,
+          prefixIcon: Icon(widget.icon),
+          suffixIcon: widget.isPassword
+              ? IconButton(
+                  onPressed: () {
+                    setState(() {
+                      isObscure = !isObscure;
+                    });
+                  },
+                  icon:
+                      Icon(isObscure ? Icons.visibility : Icons.visibility_off),
+                )
+              : null,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(18),
+          ),
+        ),
+      ),
+    );
+  }
+}
