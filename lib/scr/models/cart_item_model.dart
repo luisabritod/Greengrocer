dbb2c7d949dd6fa693fca99b2fa71906accad978
Luísa Brito <lulubritodias@gmail.com>
2024-09-16 17:07:41 -0300

    car item model

---
+++ lib/scr/models/cart_item_model.dart
@@ -0,0 +1,8 @@
+import 'package:greengrocer/scr/models/models.dart';
+
+class CartItemModel {
+  ItemModels item;
+  int quantity;
+
+  CartItemModel({required this.item, required this.quantity});
+}
--- lib/scr/models/models.dart
+++ lib/scr/models/models.dart
@@ -1,1 +1,2 @@
+export 'cart_item_model.dart';
 export 'item_models.dart';
