b3bd7929d46571660920556ccb8203d252457714
Luísa Brito <lulubritodias@gmail.com>
2024-11-11 15:17:33 -0300

    serializing the product class

--- lib/scr/models/item_models.dart
+++ lib/scr/models/item_models.dart
@@ -1,15 +1,28 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'item_models.g.dart';
+
+@JsonSerializable()
 class ItemModels {
+  String id;
+  @JsonKey(name: 'title')
   String itemName;
+  @JsonKey(name: 'picture')
   String imgUrl;
   String unit;
   double price;
   String description;
 
   ItemModels({
+    this.id = '',
     required this.itemName,
     required this.imgUrl,
     required this.unit,
     required this.price,
     required this.description,
   });
+
+  factory ItemModels.fromJson(Map<String, dynamic> json) =>
+      _$ItemModelsFromJson(json);
+  Map<String, dynamic> toJson() => _$ItemModelsToJson(this);
 }
